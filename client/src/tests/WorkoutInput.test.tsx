ee44af6c4b6141eff835cd1ee01862a2635e5e89
Ethan Ha <ethan.ha@gmail.com>
2024-11-18 12:09:16 -0800

    Forgot to add the unit test in the push.

--- client/src/tests/WorkoutInput.test.tsx
+++ client/src/tests/WorkoutInput.test.tsx
@@ -75,3 +75,42 @@ describe("Create New Workout", () => {
     expect(screen.queryByText(/Pushup/)).not.toBeInTheDocument();
   });
 });
+
+test("Delete Workout", async () => {
+  render(<App />);
+
+  const sampleWorkout = ["Dumbbell Bench Press", "Dumbbell Overhead Press", "Lateral Raise"];
+
+  const addExerciseInput = screen.getByPlaceholderText("Exercise Name");
+  const addExerciseButton = screen.getByText("Add Exercise");
+  const addWorkoutInput = screen.getByPlaceholderText("Workout Name");
+  const addWorkoutButton = screen.getByText("Add Workout");
+
+  fireEvent.change(addWorkoutInput, { target: { value: "Push Day #1" } });
+
+  // Loop through the sampleWorkout arr, add each exercise to the workout
+  for (let i = 0; i < sampleWorkout.length; i++) {
+    fireEvent.change(addExerciseInput, { target: { value: sampleWorkout[i] } });
+    fireEvent.click(addExerciseButton);
+  }
+
+  fireEvent.click(addWorkoutButton);
+
+  const newWorkout = await screen.findByText("Push Day #1");
+  const newExercise0 = await screen.findByText(/Dumbbell Bench Press/);
+  const newExercise1 = await screen.findByText(/Dumbbell Overhead Press/);
+  const newExercise2 = await screen.findByText(/Lateral Raise/);
+
+  expect(newWorkout).toBeInTheDocument();
+  expect(newExercise0).toBeInTheDocument();
+  expect(newExercise1).toBeInTheDocument();
+  expect(newExercise2).toBeInTheDocument();
+
+  const deleteWorkoutButton = screen.getByText("Delete Workout");
+  fireEvent.click(deleteWorkoutButton);
+
+  expect(screen.queryByText("Push Day #1")).not.toBeInTheDocument();
+  expect(screen.queryByText(/Dumbbell Bench Press/)).not.toBeInTheDocument();
+  expect(screen.queryByText(/Dumbbell Overhead Press/)).not.toBeInTheDocument();
+  expect(screen.queryByText(/Lateral Raise/)).not.toBeInTheDocument();
+});
