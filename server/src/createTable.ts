fcd911aa3d265fcce5409e67afde6bd976fc3291
Jonny-Le <anh.d.le2002@gmail.com>
2024-11-27 01:06:57 -0800

    Changed package.json, updated backend files
    Changed prestart to npm install instead of install i. Not a big deal, but GPT recommends npm install.
    
    Fixed createTable.ts to adhere to current frontend demands and features.
    
    Added types.ts for interfaces of backend objects
    
    Added workout-endpoints.ts

--- server/src/createTable.ts
+++ server/src/createTable.ts
@@ -9,30 +9,45 @@ const initDB = async () => {
  });
  // Create a "workouts", "exercises", "goals" table if it doesn't exist
  await db.exec(`
-   CREATE TABLE IF NOT EXISTS workouts (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        workout_name TEXT NOT NULL
-   );
+     CREATE TABLE IF NOT EXISTS workouts (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          workout_name TEXT NOT NULL
+     );
 
-    CREATE TABLE IF NOT EXISTS exercises (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        workout_id INTEGER NOT NULL,
-        exercise_name TEXT NOT NULL,
-        exercise_type TEXT NOT NULL,
-        weight_count INTEGER NOT NULL,
-        rep_count INTEGER NOT NULL,
-        distance_miles FLOAT NOT NULL,
-        duration_min FLOAT NOT NULL,
-        speed_mph FLOAT NOT NULL,
-        intensity_level TEXT NOT NULL,
-        FOREIGN KEY (workout_id) REFERENCES workouts(id) ON DELETE CASCADE
-   );
+     CREATE TABLE IF NOT EXISTS exercises_strength (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          workout_id INTEGER NOT NULL,
+          exercise_name TEXT NOT NULL,
+          set_count INTEGER DEFAULT NULL,
+          weight_count INTEGER DEFAULT NULL,
+          rep_count INTEGER DEFAULT NULL,
+          FOREIGN KEY (workout_id) REFERENCES workouts(id) ON DELETE CASCADE
+     );
 
-   CREATE TABLE IF NOT EXISTS goals (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        goal_name TEXT NOT NULL
-        isCompleted INTEGER NOT NULL
-   );
+     CREATE TABLE IF NOT EXISTS exercises_cardio (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          workout_id INTEGER NOT NULL,
+          exercise_name TEXT NOT NULL,
+          distance_miles FLOAT DEFAULT NULL,
+          duration_min FLOAT DEFAULT NULL,
+          speed_mph FLOAT DEFAULT NULL,
+          FOREIGN KEY (workout_id) REFERENCES workouts(id) ON DELETE CASCADE
+     );
+
+     CREATE TABLE IF NOT EXISTS exercises_other (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          workout_id INTEGER NOT NULL,
+          exercise_name TEXT NOT NULL,
+          duration_min FLOAT NOT NULL,
+          intensity_level TEXT DEFAULT NULL,
+          FOREIGN KEY (workout_id) REFERENCES workouts(id) ON DELETE CASCADE
+     );
+
+     CREATE TABLE IF NOT EXISTS goals (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          goal_name TEXT NOT NULL,
+          isCompleted INTEGER NOT NULL
+     );
  `);
  return db;
 };
